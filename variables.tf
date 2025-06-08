ad6df3b3aec3f59d472c86bb2ded11b5ac8e11e6
Sam <69278013+SThomson29@users.noreply.github.com>
2025-06-09 00:14:54 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable my_domain {
+  description = "My domain name variable"
+  type        = string
+}
+variable plex_claim {
+  description = "Plex claim token for authentication"
+  type        = string
+}
